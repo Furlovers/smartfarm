7e0c1e4be0efeaeddfee47f47642efd54253b57c
Enrico Santarelli <enricosantarelli@hotmail.com>
2025-06-05 18:47:42 -0300

    feat: adiciona back deployado

--- frontend/src/utils/constants/axios-instance.js
+++ frontend/src/utils/constants/axios-instance.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const createAxiosInstance = (baseURL) => {
   const instance = axios.create({ baseURL });
 
   instance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -14,11 +14,18 @@ const createAxiosInstance = (baseURL) => {
   return instance;
 };
 
-export const userAPI = createAxiosInstance('http://localhost:3000/users');
+export const userAPI = createAxiosInstance(
+  "https://smartfarm-user-mss-899314332d02.herokuapp.com/users"
+);
 
-export const sensorAPI = createAxiosInstance('http://localhost:3001/sensors');
+export const sensorAPI = createAxiosInstance(
+  "https://smartfarm-sensor-mss-60ba36d99b7f.herokuapp.com/sensors"
+);
 
-export const readingAPI = createAxiosInstance('http://localhost:3002/readings');
+export const readingAPI = createAxiosInstance(
+  "https://smartfarm-reading-mss-ee062958e049.herokuapp.com/readings"
+);
 
-export const viewAPI = createAxiosInstance('http://localhost:3003/view');
-
+export const viewAPI = createAxiosInstance(
+  "https://smartfarm-view-mss-753d9b4258f1.herokuapp.com/view"
+);
