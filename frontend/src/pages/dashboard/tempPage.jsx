338f44b871f3041915f45b4766769c79dd4f8f1b
Leonardo Fiorese <leonardogaldi.fiorese@gmail.com>
2025-06-06 20:13:59 -0300

    feat: adiciona exibição de intervalo de datas nas páginas lum, ph, temp

--- frontend/src/pages/dashboard/tempPage.jsx
+++ frontend/src/pages/dashboard/tempPage.jsx
@@ -5,6 +5,8 @@ import CustomLineChart from './components/linechart/linechart';
 import CustomList from './components/list/customList';
 import LoadingScreen from '../../components/LoadingScreen';
 import CardsGrid from '../../components/infoCards/cardsGrid';
+import { FaArrowRight } from 'react-icons/fa6';
+
 
 export default function TemperaturePage() {
   const { setSelectedIndex } = useSidebar();
@@ -25,13 +27,29 @@ export default function TemperaturePage() {
     return <LoadingScreen />;
   }
 
+  const initialDate = new Date(userData.sensorList[0].readingList[0].createdAt);
+  initialDate.setMinutes(initialDate.getMinutes() + initialDate.getTimezoneOffset());
+  const formatedInitialDate = initialDate.toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+
+  const finalDate = new Date(userData.sensorList[0].readingList[userData.sensorList[0].readingList.length - 1].createdAt);
+  finalDate.setMinutes(finalDate.getMinutes() + finalDate.getTimezoneOffset());
+  const formatedFinalDate = finalDate.toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+
   return (
     <div className="w-full h-full flex px-8 text-black flex-col">
       <div className='w-full h-60'>
         <div className='w-full h-15 bg-white rounded-md mb-4 p-2 shadow-sm hover:shadow-lg transition-all duration-300 overflow-hidden'>
-          <div className='w-1/5 h-full bg-blue-950 rounded-md flex items-center justify-center'>
-            <div className='text-white font-bold text-center w-full h-full items-center p-2'>
-              Temperatura
+          <div className='w-1/4 h-full bg-blue-950 rounded-md flex items-center justify-center'>
+            <div className='text-white font-bold text-center flex items-center justify-center gap-2'>
+              {formatedInitialDate} <FaArrowRight size={15}/> {formatedFinalDate}
               {dashboardFilterFinalData}
             </div>
           </div>
