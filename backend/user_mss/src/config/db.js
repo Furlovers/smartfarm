d2f057e5d61825511e325cc70ad368ff4c81cccb
Enrico Santarelli <enricosantarelli@hotmail.com>
2025-09-21 23:17:35 -0300

    fix: corrige scripts docker aos mss

--- backend/user_mss/src/config/db.js
+++ backend/user_mss/src/config/db.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(uri);
-    console.log('Database connected');
+    console.log("Database connected");
   } catch (err) {
-    console.error('Failed to connect to database: ', err.message);
+    console.error("Failed to connect to database: ", err.message);
     process.exit(1);
   }
 };
