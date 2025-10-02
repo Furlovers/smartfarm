ce6635ecbf68431338aca1b59e140a355f3d23de
SergioTrovo <sergio.guidi.trovo@gmail.com>
2025-10-02 08:45:11 -0300

    feat(router): adiciona as routes ao router

--- frontend_mobile/lib/app/app.dart
+++ frontend_mobile/lib/app/app.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/theme/theme.dart';
+import 'router.dart';
 
-class SmartFarmApp extends StatelessWidget {
+class SmartFarmApp extends ConsumerWidget {
   const SmartFarmApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
       title: 'SmartFarm',
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
-      home: const Scaffold(
-      ),
+      themeMode: ThemeMode.light,
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
